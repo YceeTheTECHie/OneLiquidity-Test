366772fcb361433b58b4f4a01b6f67ebb8a155c5
Boluwatife Ayodele <waplurd1@gmail.com>
2022-03-13 17:12:20 +0100

    added iam statements to give the Lambda functions read and write permissions to the  DynamoDB resource table

--- serverless.ts
+++ serverless.ts
@@ -15,6 +15,23 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    iam: {
+      role: {
+        statements: [{
+          Effect: "Allow",
+          "Allow": [
+            "dynamoDB:DescribeTable",
+            "dynamoDB:Query",
+            "dynamoDB:Scan",
+            "dynamoDB:GetItem",
+            "dynamoDB:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem"
+          ],
+        Resource: "arn:aws:dynamodb:us-west-2:*:table/Todo",
+        }]
+      }
+    }
   },
   // import the function via paths
   package: { individually: true },
