76cd57cca166501e4e7ba3da20b4d196bcf937e9
Boluwatife Ayodele <waplurd1@gmail.com>
2022-03-17 00:54:10 +0100

    added schema validation,created a reusable response handler

--- src/functions/todo/handler.ts
+++ src/functions/todo/handler.ts
@@ -3,6 +3,8 @@ import { middyfy } from '@libs/lambda';
 import { v4 } from "uuid";
 import todosService from '../../services'
 import { buildResponse } from '../../utils/buildResponse';
+import { createTodoSchema,updateTodoSchema } from "./schemas";
+
 
 
 
@@ -10,27 +12,23 @@ export const getAllTodos = middyfy(async (): Promise<APIGatewayProxyResult> => {
     
     try {
        const todos = await todosService.getAllTodos();
-        return buildResponse(200,todos,"Todos retrieved successfully!");
+        return buildResponse(200, "Todos retrieved successfully!",{},todos);
    } 
     catch (e) {
-        return buildResponse(500, e.message, "An error occured!");
+        return buildResponse(500, "An error occured!", e.errors||e.message, {});
     }
 })
 
 export const createTodo = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        await createTodoSchema.validate(event.body, { abortEarly: false });
         const id = v4();
         const todo = await todosService.createTodo({
-            id,
-            label: event.body.label,
-            completed: false,
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-
+            id,label: JSON.parse(JSON.stringify(event.body)).label,completed: false,createdAt: new Date().toISOString(),updatedAt: new Date().toISOString(),
         })
-        return buildResponse(200,todo,"Todo added successfully!");
+        return buildResponse(200, "Todo added successfully!", {}, todo);
     } catch (e) {
-        return buildResponse(500, e.message, "An error occured!");
+        return buildResponse(400, "An error occured!", e.errors||e.message,{});
     }
 })
 
@@ -38,21 +36,27 @@ export const getTodo = middyfy(async (event: APIGatewayProxyEvent): Promise<APIG
     const id = event.pathParameters.id;
     try {
         const todo = await todosService.getTodo(id)
-        return buildResponse(200, todo, "Todo retrieved successfully!");
+        return buildResponse(200, "Todo retrieved successfully!", todo, {});
     } catch (e) {
-        return buildResponse(500, e.message, "An error occured!");
+        return buildResponse(400,"An error occured!",e.errors||e.message, {});
     }
 })
 
 export const updateTodo = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const id = event.pathParameters.id;
-    const { label, completed } = event.body; 
+    const { label, completed } = JSON.parse(JSON.stringify(event.body)); 
     const updatedAt = new Date().toISOString()
     try {
+        await updateTodoSchema.validate(event.body, { abortEarly: false });
+    }
+    catch (e) {
+        return buildResponse(400, "Validation Failed!",e.errors, {});
+        }
+    try {
         const todo = await todosService.updateTodo(id, { completed, label, updatedAt });
-        return buildResponse(200, todo, "Todo updated successfully!");
+        return buildResponse(200,"Todo updated successfully!",{},todo);
     } catch (e) {
-        return buildResponse(500, e.message, "An error occured!");
+        return buildResponse(500, "An error occured!", e.message, {});
     }
 })
 
@@ -60,8 +64,8 @@ export const deleteTodo = middyfy(async (event: APIGatewayProxyEvent): Promise<A
     const id = event.pathParameters.id;
     try {
         const todo = await todosService.deleteTodo(id)
-        return buildResponse(200, todo, "Todo deleted successfully!");
+        return buildResponse(200, "Todo deleted successfully!",  todo, {});
     } catch (e) {
-        return buildResponse(500, e.message, "An error occured!");
+        return buildResponse(500,"An error occured!", e.errors||e.message,{} );
     }
 })
--- src/utils/buildResponse.ts
+++ src/utils/buildResponse.ts
@@ -2,11 +2,17 @@ import { formatJSONResponse } from '@libs/api-gateway';
 
 
 
-export const buildResponse =  (statusCode, data,message) => {
-  return formatJSONResponse({
-    statusCode: statusCode,
-    message,
-    data
+export const buildResponse = (statusCode: number,message:string,error:object,data:object) => {
+    switch (statusCode) {
+        case 200:
+            return formatJSONResponse({ statusCode: statusCode, message, error,data})
+            break;
+        case 400:
+            return formatJSONResponse({ statusCode, message, error,data })
+            break;
+        // add more cases if need be
+        default:
+            return formatJSONResponse({ statusCode: statusCode, message, error,data })
+    }
   }
-  )}
 
