7dc7d14f089761fd64b78199300ab0be36d24aa1
Boluwatife Ayodele <waplurd1@gmail.com>
2022-03-16 18:07:14 +0100

    routed the todo/ route to the getAllTodos lambda function with the correct http method

---
+++ src/functions/todo/index.ts
@@ -0,0 +1,13 @@
+import { handlerPath } from '@libs/handler-resolver';
+
+export const getAllTodos = {
+    handler: `${handlerPath(__dirname)}/handler.getAllTodos`,
+    events: [
+        {
+            http: {
+                method: 'get',
+                path: 'todo/',
+            },
+        },
+    ],
+};
