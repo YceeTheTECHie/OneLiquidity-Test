75d5cebdc0b5f2ebc8477933cf783aae2c14fd18
Boluwatife Ayodele <waplurd1@gmail.com>
2022-03-18 18:00:02 +0100

    added unit test

---
+++ test/unit/todo.ts
@@ -0,0 +1,22 @@
+export const data = {
+    "statusCode": 200,
+    "message": "Todos retrieved successfully!",
+    "error": {},
+    "data": [
+        {
+            "createdAt": "2022-03-18T14:29:32.098Z",
+            "id": "62efee2a-93f3-491b-a180-4d05f3b0947e",
+            "label": "firing",
+            "completed": false,
+            "updatedAt": "2022-03-18T14:29:32.098Z"
+        },
+   
+        {
+            "createdAt": "2022-03-18T13:15:55.555Z",
+            "id": "81ee1b09-4adf-40c1-a6c1-d3129243a058",
+            "label": "check garden",
+            "completed": false,
+            "updatedAt": "2022-03-18T13:15:55.555Z"
+        }
+    ]
+}
